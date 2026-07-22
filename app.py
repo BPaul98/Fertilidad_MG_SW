caec035be36ae8b9cb31a6ebe0efc99ca4003a48
BPaul98 <bryanlascano1c@gmail.com>
2026-07-22 13:36:47 -0500

    Refactor app.py to improve model loading and UI

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ st.markdown("Plataforma de procesamiento masivo para bases de datos litogeoquím
 
 @st.cache_resource
 def cargar_modelos():
-    scaler = joblib.load('scaler_geoquimico.pkl')
-    modelo = load_model("modelo_fertilidad.pkl", compile=False)
+    scaler = joblib.load('modelo_fertilidad.pkl')
+    modelo = load_model("modelo_fertilidad.h5", compile=False)
     return scaler, modelo
 
 scaler, modelo_nn = cargar_modelos()
